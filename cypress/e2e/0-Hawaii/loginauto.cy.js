474c80edfe0a378eee7e61187de48768c862a0c1
raschetov58 <raschetov58@gmail.com>
2025-04-28 10:38:43 +0300

    add email

--- cypress/e2e/0-Hawaii/loginauto.cy.js
+++ cypress/e2e/0-Hawaii/loginauto.cy.js
@@ -1,5 +1,10 @@
 describe('Проверка формы логина', function() {
-    it ('Позитивный кейс: верный логин, верный пароль', function() {
-      cy.visit ('https://login.qa.studio/');
+    
+  
+  it ('Позитивный кейс: верный логин, верный пароль', function() {
+      cy.visit('https://login.qa.studio/');
+      cy.get('#mail').type('german@dolnikov.ru');
+      cy.get('#loginButton').should('be.disabled');
+      
     })
   })
