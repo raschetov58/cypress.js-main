3cf2c2d8e5089c0b6d4472e7b8e2eaf38804761f
raschetov58 <raschetov58@gmail.com>
2025-04-28 12:41:07 +0300

    Негативный кейс

--- cypress/e2e/0-Hawaii/loginauto.cy.js
+++ cypress/e2e/0-Hawaii/loginauto.cy.js
@@ -11,6 +11,18 @@ describe('Проверка формы логина', function() {
 
       cy.contains('Авторизация прошла успешно');
       cy.get('#exitMessageButton > .exitIcon')
-      
-    })
+   })
+
+   it ('Негативный кейс: верный логин, неверный пароль', function() {
+      cy.visit('https://login.qa.studio/');
+      cy.get('#mail').type('german@dolnikov.ru');
+      cy.get('#loginButton').should('be.disabled');
+      cy.get('#pass').type('iLoveqastudio2');
+      cy.get('#loginButton').should('not.be.disabled');
+      cy.get('#loginButton').click();
+
+      cy.contains('Такого логина или пароля нет');
+   
+ })
+
   })
